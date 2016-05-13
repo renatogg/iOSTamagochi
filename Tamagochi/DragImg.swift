f0b0ef1b566234eeb236e59f9e8b124873d2d118
Renato Girardi Gasoto <rggasoto@wpi.edu>
2016-05-12 22:11:42 -0400

    Draggable Heart and food

--- Tamagochi/DragImg.swift
+++ Tamagochi/DragImg.swift
@@ -10,6 +10,9 @@ import Foundation
 import UIKit
 
 class DragImg: UIImageView{
+    
+    var originalPosition: CGPoint!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -19,12 +22,15 @@ class DragImg: UIImageView{
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
+        originalPosition = self.center
     }
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    
+        if let touch = touches.first {
+            let position = touch.locationInView(self.superview)
+            self.center = CGPointMake(position.x, position.y)
+        }
     }
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
+        self.center = originalPosition
     }
 }
